cef13ad2c0e3736ad84eb0b017a6643b67dd63ff
Hackbright Student <info@hackbrightacademy.com>
2015-05-05 12:07:24 -0700

    added preliminary routes for flask and base.html, homepage, user_list, also linked tables through setting foreign keys and backref

--- model.py
+++ model.py
@@ -50,9 +50,15 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     score = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship("User",
+                            backref=db.backref("ratings", order_by=rating_id))
+
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings", order_by=rating_id))
 
     def __repr__(self):
         """Provide helpful representation when printed"""
--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,14 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show a list of users"""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
 
 
 if __name__ == "__main__":
