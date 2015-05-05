415a0257da194868434d1c62fe28a537ac039c47
Hackbright Student <info@hackbrightacademy.com>
2015-05-04 17:30:40 -0700

    Fixed bugs in load_user and load_movies function

--- seed.py
+++ seed.py
@@ -2,7 +2,7 @@
 
 from model import User, Rating, Movie, connect_to_db, db
 from server import app
-from datetime import datetime, strptime
+import datetime
 
 def load_users():
     """Load users from u.user into database."""
@@ -11,7 +11,7 @@ def load_users():
 
     for line in users_file:
         line = line.strip().split("|")
-        a_user = User(user_id = line[0], age = line[1], zipcode = line[4])
+        a_user = User(age = line[1], zipcode = line[4])
         db.session.add(a_user)
 
     db.session.commit()
@@ -20,13 +20,20 @@ def load_users():
 def load_movies():
     """Load movies from u.item into database."""
 
-    movies_file = open('/seed_data/u.item')
+    movies_file = open('./seed_data/u.item')
 
     for line in movies_file:
-        line = line.strip.split("|")
-        date = " ".join(line[2].split("-"))
-        date = datetime.strptime(date, '%b, %d, %Y')
-        # a_movie = Movie(movie_id=line[0], movie_title=line[1][:-7], released_at=line[])
+        line = line.strip().split("|")
+        if line[2] == "":
+            date = None
+        else:
+            date = datetime.datetime.strptime(line[2], '%d-%b-%Y')
+        a_movie = Movie(
+            movie_id=line[0], movie_title=line[1][:-7],
+            released_at=date, imdb_url=line[4])
+        db.session.add(a_movie)
+
+    db.session.commit()
 
 
 def load_ratings():
